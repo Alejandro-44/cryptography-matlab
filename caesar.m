42ea86750a210da4703c30cc93e982c09bb542ac
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-04-27 23:58:17 -0500

    Changed algorithm to be case insensitive

--- caesar.m
+++ caesar.m
@@ -1,11 +1,16 @@
 
 function cipher_text =  caesar_cipher(clear_text, key)
-    clear_text = upper(clear_text);
     cipher_text = zeros(1, length(clear_text));
     for i = 1:length(clear_text)
         if isletter(clear_text(i))
             ascii_code = double(clear_text(i));
-            cipher_code = mod(ascii_code - 65 + key, 26) + 65;
+            base = 0;
+            if ascii_code >= 65 && ascii_code <= 90
+                base = 65;
+            elseif ascii_code >= 97 && ascii_code <= 122
+                base = 97;
+            end
+            cipher_code = mod(ascii_code - base + key, 26) + base;
             cipher_character = char(cipher_code);
             cipher_text(i) = cipher_character;
         else
@@ -16,7 +21,7 @@ function cipher_text =  caesar_cipher(clear_text, key)
 end
 
 % Use example
-texto_original = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+texto_original = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz';
 clave = 3;
 texto_cifrado = caesar_cipher(texto_original, clave);
 disp(texto_cifrado);
