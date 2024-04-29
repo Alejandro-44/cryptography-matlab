7522d51772cfb2ee955f0fad21e7a7127fb2e897
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-04-28 21:01:45 -0500

    Reduced Caesar only for English alphabet

--- caesar.m
+++ caesar.m
@@ -1,17 +1,16 @@
-function cipher_text =  caesar(clear_text, key, is_spanish)
-    % CAESAR Apply caesar cipher in a text for English and Spanish alphabet.
+function cipher_text =  caesar(clear_text, key)
+    % CAESAR Apply caesar cipher in a text for English alphabet.
     %   cipher_text = CAESAR(clear_text, key, is_spanish) 
     % Params
     %   clear_next -> str: message text to cipher 
     %   key        -> int: offset value
-    %   is_spanish -> bool: use Sanish alphabet or not
+    %
     %
     %   Example:
-    %       clear_text = 'Hola';
+    %       clear_text = 'Hello';
     %       key = 3;
-    %       is_spanish = 1;
-    %       cipher_text = caesar(clear_text, key, is_spanish);
-    %       OUT: Kqñd
+    %       cipher_text = caesar(clear_text, key);
+    %       OUT: Khoor
 
     % create a new zero vector for result 
     cipher_text = zeros(1, length(clear_text));
@@ -21,31 +20,13 @@ function cipher_text =  caesar(clear_text, key, is_spanish)
         if isletter(clear_text(i))
             ascii_code = double(clear_text(i)); % get  letter ascii code
             base = 0; % initial code (use to normalize values)
-            breakdown = 0; % character code where the ñ would be found 
-            spe_char = 0; % ascii code for ñ
-            % if the letter is upercase or is Ñ
             if (ascii_code >= 65 && ascii_code <= 90) || ascii_code == 209
-                base = 65; 
-                breakdown = 79; 
-                spe_char = 209; 
-            % if the letter is lowercase or is ñ
+                base = 65;
             elseif (ascii_code >= 97 && ascii_code <= 122) || ascii_code == 241
-                base = 97; % initial lowercase letter ascii code 
-                breakdown = 111;
-                spe_char = 241;
+                base = 97; % initial lowercase letter ascii code
             end
             % calculate new ascii code by the key
-            cipher_code = mod(ascii_code - base + key, 26) + base; 
-            % if choose encryption with Spanish alphabet
-            if is_spanish
-                % if the letter is the one that will be replaced by Ñ
-                if ascii_code == (breakdown - key)
-                    cipher_code = spe_char;
-                % if the letter is between the previus one and Ñ
-                elseif ascii_code > (breakdown - key) && ascii_code <= breakdown
-                    cipher_code = mod(ascii_code - base + key, 26) + base - 1;
-                end
-            end
+            cipher_code = mod(ascii_code - base + key, 26) + base;
             % replace a zero for cipher code in the same position
             cipher_text(i) = cipher_code;
         else
--- try_algorithms.m
+++ try_algorithms.m
@@ -1,3 +1,3 @@
-clear_text = 'Hola Mundo';
-cipher_text = caesar(clear_text, 3, 1);
-disp(cipher_text)
+text = 'Hello World';
+cipher_text = caesar(text, 3);
+disp(cipher_text);
